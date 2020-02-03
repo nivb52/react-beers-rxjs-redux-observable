6363398a287e0826d23f78514715b278a0a8a628
Niv B <nivb52@gmail.com>
2020-02-03 14:53:19 +0200

    adding store

---
+++ src/configureStore.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { appReducer } from "./reducers/appReducer";
+// import reducer from './reducers'
+
+import { combineEpics, createEpicMiddleware } from "redux-observable";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
+
+import { fetchBeersEpic } from "./epics/fetchBeers";
+
+export function configureStore() {
+  const rootEpic = combineEpics(fetchBeersEpic);
+
+  const epicMiddleware = createEpicMiddleware();
+
+  const rootReducer = combineReducers({
+    app: appReducer
+  });
+
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(epicMiddleware))
+  );
+  epicMiddleware.run(rootEpic);
+
+  return store;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { configureStore } from "./configureStore";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
