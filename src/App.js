a917221b595330187db550d6a9913d8632eeffe3
Niv B <nivb52@gmail.com>
2020-02-03 18:12:13 +0200

    move css to sperate folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import "./App.css";
+import "./css/App.css";
 
 import { connect } from "react-redux";
+import Beers from "./components/Beers";
 
 function App(props) {
-
-  return <div className="App">REACT - by {props.app.name}</div>;
+  return (
+    <div className="App">
+      <div className="centered">
+      <h1> Beers App </h1>
+      <span className="subtitle">by {props.app.name}</span>
+      </div>
+      <Beers />
+    </div>
+  );
 }
 
 export default connect(state => state)(App);
