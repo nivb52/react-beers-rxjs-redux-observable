65d47ea09a6936f2f231b7a0f72c6736c2fcdb28
Niv B <nivb52@gmail.com>
2020-02-05 01:46:17 +0200

    got control in case of not found

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 
-export function BeerList({beers}) {
+export function BeerList({ beers }) {
   return (
     <ul className="List">
-      {beers.map(beer => {
-        return (
-          <li key={beer.id} className="List-item">
-            <figure className="List-item-img">
-              <img src={beer.image_url} alt="beer" />
-            </figure>
-            <div className="List-item-info">
-              <p>{beer.name}</p>
-              <ul>
-                <li>
-                  <small>ABV: {beer.abv}</small>
-                </li>
-                <li>
-                  <small>
-                    Volume: {beer.volume.unit} {beer.volume.unit}
-                  </small>
-                </li>
-              </ul>
-            </div>
-          </li>
-        );
-      })}
+      {beers && !beers[0] && <p className="centered"> No beers found </p>}
+      {beers &&
+        beers[0] &&
+        beers.map(beer => {
+          return (
+            <li key={beer.id} className="List-item">
+              <figure className="List-item-img">
+                <img src={beer.image_url} alt="beer" />
+              </figure>
+              <div className="List-item-info">
+                <p>{beer.name}</p>
+                <ul>
+                  <li>
+                    <small>ABV: {beer.abv}</small>
+                  </li>
+                  <li>
+                    <small>
+                      Volume: {beer.volume.unit} {beer.volume.unit}
+                    </small>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          );
+        })}
     </ul>
   );
 }
