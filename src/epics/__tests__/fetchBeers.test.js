1f1d3aeaa94be04949d2c88698d8404a7033a0e7
Niv B <nivb52@gmail.com>
2020-02-07 13:58:11 +0200

    add test to failure case

--- src/epics/__tests__/fetchBeers.test.js
+++ src/epics/__tests__/fetchBeers.test.js
@@ -2,52 +2,73 @@
 import { searchBeerEpic } from "../fetchBeers";
 import { OPTIONS } from "../../reducers/optionsActions";
 import { initialState } from "../../reducers/optionsReducer";
-import { setStatus, fetchFulfilled , search, PENDING} from "../../reducers/beersActions";
+import {
+  setStatus,
+  fetchFulfilled,
+  search,
+  PENDING,
+  fetchFailed
+} from "../../reducers/beersActions";
 // node modules imports
 import { of } from "rxjs";
 import { TestScheduler } from "rxjs/testing";
 
-it("produces correct actions (success)", function() {
-    const testScheduler = new TestScheduler((actual, expected) => {
-        expect(actual).toEqual(expected);
-    });
+it("produces search actions (success)", function() {
+  const testScheduler = new TestScheduler((actual, expected) => {
+    expect(actual).toEqual(expected);
+  });
 
-    testScheduler.run(({ hot, cold, expectObservable }) => {
-        // user activate this action of the store:
-        const action$ = hot('a', {
-            a: search("beer")
+  testScheduler.run(({ hot, cold, expectObservable }) => {
+    // user activate this action of the store:
+    const action$ = hot("a", {
+      a: search("beer")
+    });
+    // the current state to pass the function:
+    const state$ = of({
+      [OPTIONS]: initialState
+    });
+    // test dependencies
+    const dependencies = {
+      getJSON: url => {
+        // cold obser for dependencies , get: (frame,data)
+        return cold("-a", {
+          a: [{ name: "Goldstar Beer" }]
         });
-        // the current state to pass the function:
-        const state$ = of({
-            [OPTIONS]: initialState
-        });
-        // test dependencies
-        const dependencies = {
-            getJSON: (url) => {
-                // cold obser for dependencies
-                return cold('-a', {
-                    a: [{name: "Goldstar Beer"}]
-                })
-            }
-        };
-        const output$ = searchBeerEpic(action$, state$, dependencies);
-        expectObservable(output$).toBe('500ms ab', {
-            a: setStatus(PENDING),
-            b: fetchFulfilled([{name: "Goldstar Beer"}])
-        })
-    })
+      }
+    };
+    const output$ = searchBeerEpic(action$, state$, dependencies);
+    expectObservable(output$).toBe("500ms ab", {
+      a: setStatus(PENDING),
+      b: fetchFulfilled([{ name: "Goldstar Beer" }])
+    });
+  });
 });
 
+it("produces search actions (failure) ", () => {
+  const ts = new TestScheduler((actual, expected) => {
+    expect(actual).toEqual(expected);
+  });
 
-it("produces correct actions (failure) ", () => {
-    const ts = new TestScheduler( (actual, expected) => {
-        expected(actual).toEqual(expected)
-    })
-
-    ts.run( ({hot, cold, expectObservable}) => {
-        const action$ =  hot('a', {a : search("beer")})
-        const state$
-        const dependencies 
-    }
-    )
-}
+  ts.run(({ hot, cold, expectObservable }) => {
+    const action$ = hot("a", { a: search("beer") });
+    const state$ = of({
+      [OPTIONS]: initialState
+    });
+    const dependencies = {
+      // here we want to create the error :
+      // '#' for error, null for data, and error Obj
+      getJSON: url => {
+        return cold("-#", null, {
+          response: {
+            message: "some error text"
+          }
+        });
+      }
+    };
+    const output$ = searchBeerEpic(action$, state$, dependencies);
+    expectObservable(output$).toBe("500ms ab", {
+      a: setStatus(PENDING),
+      b: fetchFailed({ text: "some error text" })
+    });
+  });
+});
