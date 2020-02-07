d85805c205077ec7957469c1e65be650555f28f5
Niv B <nivb52@gmail.com>
2020-02-07 15:33:54 +0200

    Updates

---
+++ precache-manifest.c690dff2b26cd55c8f922129911905e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ff1970ee1e94478a87f5876adbf2a8",
+    "url": "/react-beers-rxjs-redux-observable/index.html"
+  },
+  {
+    "revision": "0d82cfa8cf854739793d",
+    "url": "/react-beers-rxjs-redux-observable/static/css/main.19400fc1.chunk.css"
+  },
+  {
+    "revision": "21f24842f4e8026c17d6",
+    "url": "/react-beers-rxjs-redux-observable/static/js/2.2d4d2a96.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/react-beers-rxjs-redux-observable/static/js/2.2d4d2a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d82cfa8cf854739793d",
+    "url": "/react-beers-rxjs-redux-observable/static/js/main.516bfd4e.chunk.js"
+  },
+  {
+    "revision": "4d17c1022eb79db6a7a8",
+    "url": "/react-beers-rxjs-redux-observable/static/js/runtime-main.1d96af62.js"
+  }
+]);
