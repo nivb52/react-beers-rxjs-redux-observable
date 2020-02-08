d0f7406eb4f3bacda495904db588289e3cf7a473
Niv B <nivb52@gmail.com>
2020-02-08 21:04:46 +0200

    change loading location for online web

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -114,7 +114,7 @@ export function Beers({
 
       {status === "PENDING" && (
         <span className="App-spinner centered">
-          <img src={"/beer-loader.gif"} className="beer-loader" alt="loading" />
+          <img src={window.location.origin +"/beer-loader.gif"} className="beer-loader" alt="loading" />
         </span>
       )}
 
